4828be3034e1998c82822f27fe064599150b288f
telesers <alireza7985@outlook.com>
2016-05-23 13:39:13 +0430

    Update ads.lua

--- plugins/ads.lua
+++ plugins/ads.lua
@@ -1,10 +1,8 @@
 do
  function run(msg, matches)
 return [[ 
-کانال رسمی تیم بزرگ آواست!جدید ترین نوآوری ها و... 
-https://telegram.me/avast_team
-https://telegram.me/avast_team
-https://telegram.me/avast_team
+ ايدي بات 
+@puoker
 ]]
 end
 return {
